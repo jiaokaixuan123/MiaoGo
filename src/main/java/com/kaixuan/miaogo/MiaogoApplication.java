31e648e8c63ab41c812a44a3de7e0b63932e1a88
jiaokaixuan123 <1625988679@qq.com>
2026-06-15 22:51:46 +0800

    构建项目

---
+++ src/main/java/com/kaixuan/miaogo/MiaogoApplication.java
@@ -0,0 +1,13 @@
+package com.kaixuan.miaogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaogoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiaogoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaixuan/miaogo/MiaogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaixuan.miaogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaogoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
